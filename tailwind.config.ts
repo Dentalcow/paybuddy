3de816492c4ee99a27e234d42989590aa47a10c4
DentalCow <87558010+Dentalcow@users.noreply.github.com>
2023-06-06 14:50:56 +1000

    idk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["winter"],
+  },
 } satisfies Config;
